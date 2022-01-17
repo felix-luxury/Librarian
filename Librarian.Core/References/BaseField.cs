26e5a6bc00b4b090f488dbc16079b1e1364a2ab2
Bogdan <kulikovsky1999@mail.ru>
2022-01-17 19:17:55 +0300

    Привели в порядок папки

--- Librarian.Core/References/BaseField.cs
+++ Librarian.Core/References/BaseField.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Librarian.Core.LiterarySource
+namespace Librarian.Core.References
 {
     public abstract class BaseField
     {

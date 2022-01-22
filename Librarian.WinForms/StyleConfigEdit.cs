a86edbe9c6190c1816ffadf1adeb4afccf2ad7c2
Bogdan <kulikovsky1999@mail.ru>
2022-01-22 15:01:43 +0300

    Добавил TextBox(ы) и Label(ы) для редактирования конфигов стилей

--- Librarian.WinForms/StyleConfigEdit.cs
+++ Librarian.WinForms/StyleConfigEdit.cs
@@ -16,5 +16,60 @@ namespace Librarian.WinForms
         {
             InitializeComponent();
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
